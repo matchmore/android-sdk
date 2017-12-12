a001e8b5a49057e12dc61ccdba6c6b73fee9aa94
Maciej Burda <maciej.burda@me.com>
2017-12-12 13:54:18 +0100

    added simple match monitor

---
+++ sdk/src/main/java/io/matchmore/sdk/monitoring/MatchMonitor.kt
@@ -0,0 +1,50 @@
+package io.matchmore.sdk.monitoring
+
+import io.matchmore.sdk.AlpsManager
+import io.matchmore.sdk.api.async
+import io.matchmore.sdk.api.models.Device
+import io.matchmore.sdk.api.models.Match
+import java.util.*
+
+interface MatchMonitorDelegate {
+    fun didReceiveMatches(matches: Set<Match>, forDevice: Device)
+}
+
+class MatchMonitor(private val manager: AlpsManager) {
+    var monitoredDevices = mutableSetOf<Device>()
+    var deliveredMatches = mutableSetOf<Match>()
+
+    var timer: Timer? = null
+
+    fun startPollingMatchesA() {
+        val delay: Long = 0 // delay for 0 sec.
+        val period: Long = 10000 // repeat every 10 sec.
+        timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                getMatches()
+            }
+        }, delay, period)
+    }
+
+    fun stopPollingMatches(): Void {
+        timer?.cancel()
+        timer = null
+    }
+
+    private fun getMatches() {
+        monitoredDevices.forEach {
+            getMatchesForDevice(it)
+        }
+    }
+
+    private fun getMatchesForDevice(device: Device) {
+        manager.apiClient.matchesApi.getMatches(device.id).async({
+            val union = deliveredMatches.union(it.toSet())
+            if union != deliveredMatches {
+                deliveredMatches = union
+                // notify delegates
+            }
+        })
+    }
+}
