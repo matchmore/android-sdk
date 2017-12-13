9fa9c3b753eb65b465dfe34a603f9ae3ba322589
Maciej Burda <maciej.burda@me.com>
2017-12-13 13:14:12 +0100

    improved match monitor

--- sdk/src/main/java/io/matchmore/sdk/monitoring/MatchMonitor.kt
+++ sdk/src/main/java/io/matchmore/sdk/monitoring/MatchMonitor.kt
@@ -6,24 +6,34 @@ import io.matchmore.sdk.api.models.Device
 import io.matchmore.sdk.api.models.Match
 import java.util.*
 
-interface MatchMonitorDelegate {
-    fun didReceiveMatches(matches: Set<Match>, forDevice: Device)
+interface MatchMonitorListener {
+    fun onReceiveMatches(matches: Set<Match>, forDevice: Device)
 }
 
-class MatchMonitor(private val manager: AlpsManager, private val delegate: MatchMonitorDelegate) {
+class MatchMonitor(private val manager: AlpsManager) {
     var monitoredDevices = mutableSetOf<Device>()
     var deliveredMatches = mutableSetOf<Match>()
+
+    private var listeners = mutableSetOf<MatchMonitorListener>()
+
+    fun addOnMatchListener(listener: MatchMonitorListener) {
+        listeners.add(listener)
+    }
+
+    fun removeOnMatchListener(listener: MatchMonitorListener) {
+        listeners.remove(listener)
+    }
 
     private var timer: Timer? = null
 
     fun startPollingMatchesA() {
-        val period: Long = 5000 // repeat every 10 sec.
+        if (timer != null) { return }
         timer = Timer()
         timer?.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
                 getMatches()
             }
-        }, 0, period)
+        }, 0, 5000) // repeat every 5 sec.
     }
 
     fun stopPollingMatches() {
@@ -31,19 +41,17 @@ class MatchMonitor(private val manager: AlpsManager, private val delegate: Match
         timer = null
     }
 
-    private fun getMatches() {
-        monitoredDevices.forEach {
-            getMatchesForDevice(it)
-        }
-    }
+    private fun getMatches() = monitoredDevices.forEach { getMatchesForDevice(it) }
 
     private fun getMatchesForDevice(device: Device) {
         device.id?.let {
-            manager.apiClient.matchesApi.getMatches(it).async({
-                val union = deliveredMatches.union(it.toSet())
-                if (!(deliveredMatches == union)) {
-                    deliveredMatches = union.toMutableSet()
-                    delegate?.didReceiveMatches(union, device)
+            manager.apiClient.matchesApi.getMatches(it).async({ matches ->
+                val newMatches = matches - deliveredMatches
+                if (newMatches.isNotEmpty()) {
+                    deliveredMatches.addAll(newMatches)
+                    listeners.forEach {
+                        it.onReceiveMatches(newMatches.toSet(), device)
+                    }
                 }
             }, null)
         }
